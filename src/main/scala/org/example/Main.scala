c52321ec45b2d768ef4e958b68484b658e5b9985
Hugo0322 <hugito2003@gmail.com>
2024-02-20 08:17:17 +0100

    Creating SQL Databases and Tables

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -3,7 +3,7 @@ package org.example
 
 import org.example.chapter3.{Ej1_DefiningSchema, Ej2_FireCalls_CommonDataFrameOperations, Ej3_DataSet_API}
 import org.example.chapter2.Ej1_MnMs
-import org.example.chapter4.Ej1_BasicSQLQueryExamples
+import org.example.chapter4.{Ej1_BasicSQLQueryExamples, Ej2_CreatingSQLDatabasesAndTables}
 
 object Main {
   def main(args:Array[String]): Unit = {
@@ -15,31 +15,36 @@ object Main {
 
     /*
       // Chapter2 Ej1_MnMs execution code
-      val mnmDataSet_File = "src/main/resources/mnm_dataset.csv"
+      val mnmDataSet_File = "src/main/resources/in/mnm_dataset.csv"
       Ej1_MnMs.run(mnmDataSet_File)
      */
 
     /*
       // Chapter3 Ej1_DefiningSchema execution code
-      val blogs_File = "src/main/resources/blogs.json"
+      val blogs_File = "src/main/resources/in/blogs.json"
       DefiningSchema.run(blogs_File)
      */
 
     /*
       // Chapter3 Ej2_FireCallsCommonDataFrameOperations execution code
-      val sfFireCalls_File = "src/main/resources/sf-fire-calls.csv"
+      val sfFireCalls_File = "src/main/resources/in/sf-fire-calls.csv"
       Ej2_FireCalls_CommonDataFrameOperations.run(sfFireCalls_File)
      */
 
     /*
       // Chapter3 Ej3_DataSet_API execution code
-      val scalaCaseClasses_File = "src/main/resources/iot_devices.json"
+      val scalaCaseClasses_File = "src/main/resources/in/iot_devices.json"
       Ej3_DataSet_API.run(scalaCaseClasses_File)
      */
 
-    // Chapter4  execution code
-    val basicSQLQueryExamples = "src/main/resources/departuredelays.csv"
-    Ej1_BasicSQLQueryExamples.run(basicSQLQueryExamples)
+    /*
+      // Chapter4 Basic SQL Exmaples execution code
+      val basicSQLQueryExamples = "src/main/resources/in/departuredelays.csv"
+      Ej1_BasicSQLQueryExamples.run(basicSQLQueryExamples)
+     */
 
+    // Chapter4 Creating SQL Databases and Tables execution code
+    val creatingSQLDatabasesAndTables = "src/main/resources/in/departuredelays.csv"
+    Ej2_CreatingSQLDatabasesAndTables.run(creatingSQLDatabasesAndTables)
   }
 }
--- src/main/scala/org/example/commonlyUsedSharedFolder/sparkSessionFactory/SparkSessionFactory.scala
+++ src/main/scala/org/example/commonlyUsedSharedFolder/sparkSessionFactory/SparkSessionFactory.scala
@@ -9,9 +9,15 @@ object SparkSessionFactory {
       .builder()
       .appName(appName)
       .master("local")
+      //.enableHiveSupport()
+      //.config("spark.sql.catalogImplementation","hive")
+      .config("spark.executor.memory", "2g")
+      .config("spark.executor.instances", "4")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
 
-      spark.sparkContext.setLogLevel("ERROR")
+
+    spark.sparkContext.setLogLevel("ERROR")
 
       spark
   }
